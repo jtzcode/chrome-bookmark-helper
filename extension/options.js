1102ea467a440826ebfbebc98e3512e1552b0f9f
Tianze Jiang <tianze.jiang@citrix.com>
2019-09-11 17:09:58 +0800

    Use OAuth to get token - part 1

--- extension/options.js
+++ extension/options.js
@@ -1,46 +1,58 @@
-/* // Saves options to chrome.storage
-function save_options() {
-    chrome.storage.sync.set({
-      favoriteColor: "color",
-      likesColor: "likesColor"
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
+
+var callbackUrl = chrome.extension.getURL("oauth.html"); // your endpoint
+let oauthToken = "";
+let oauthTokenSecret = "";
+
+// initialize OAuth
+var Evernote = require('evernote');
+const https = require('https');
+
+var client = new Evernote.Client({
+  consumerKey: 'jaxpression-7658',
+  consumerSecret: '784a540726be2a0f',
+  sandbox: false, // change to false when you are ready to switch to production
+  china: true, // change to true if you wish to connect to YXBJ - most of you won't
+  serviceHost: "sandbox.yinxiang.com"
+});
+
+client.getRequestToken(callbackUrl, function(error, token, tokenSecret) {
+    console.log(callbackUrl);
+    if (error) {
+      // do your error handling here
+    }
+    // store your token here somewhere - for this example we use req.session
+    console.log(token + " " +  tokenSecret)
+    oauthToken = token;
+    oauthTokenSecret = tokenSecret;
+    window.location.href = client.getAuthorizeUrl(oauthToken); // send the user to Evernote
+  });
+
+
+
+/* client.getAccessToken(req.session.oauthToken,
+  req.session.oauthTokenSecret,
+  req.query.oauth_verifier,
+function(error, oauthToken, oauthTokenSecret, results) {
+  if (error) {
+    // do your error handling
+  } else {
+    // oauthAccessToken is the token you need;
+    var authenticatedClient = new Evernote.Client({
+      token: oauthToken,
+      sandbox: true,
+      china: false,
+    });
+    var noteStore = authenticatedClient.getNoteStore();
+    noteStore.listNotebooks().then(function(notebooks) {
+      console.log(notebooks); // the user's notebooks!
     });
   }
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.getElementById('save').addEventListener('click',
-      save_options); */
+}); */
 
-let evernoteStore;
+let authorizeNoteApp = () => {
 
-let restore_options = () => {
-    chrome.storage.sync.get({
-        storage_type: 'evernote'
-    }, (items) => {
-        let type = items.storage_type;
-        // Currently, only Evernote is supported.
-        if (type === 'evernote') {
-            // Check if evernote account is authenticated with token.
-            // If authenticated, load the notebooks.
-            chrome.storage.sync.get({
-                targetBook: '',
-                targetNote: ''
-              }, (results) => {
-                targetBook = results.targetBook;
-                targetNote = results.targetNote;
-            });
-        }
-        // else load options of other storage types
-    });
 };
+
 
 $(() => {
 
